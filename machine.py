4ec8e9f0bafe8b0828f5bbe2249a9304b0c13afa
jonibek99 <jonibekannaev5@gmail.com>
2026-03-25 06:30:45 +0500

    solo_push

--- machine.py
+++ machine.py
@@ -1,16 +1,23 @@
 from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
 import pandas as pd
 import numpy as np
 
-df=pd.read_csv('house.csv')
-X = df.drop('price_$', axis=1) 
-y = df["price_$"] 
+df = pd.read_csv('house.csv')
 
+X = df.drop('price_$', axis=1)  
+y = df['price_$']
+
+# To'g'ri train-test split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Model yaratish
 linear_regression = LinearRegression()
 
+# Modelni o‘qitish
+linear_regression.fit(X_train, y_train)
 
-linear_regression.fit(X, y)
+# Bashorat qilish
+predict = linear_regression.predict(X_test)
 
-predick=linear_regression.predict(df)
-
-print(predick)
+print(predict)
